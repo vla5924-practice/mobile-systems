7e186b40ec655ddd23cb335536363857ddc3907e
vla5924 <vla5924@gmail.com>
2021-10-31 14:14:15 +0300

    renamed report file

--- reports/render.cmd
+++ reports/render.cmd
@@ -2,6 +2,6 @@
 
 del /q output\*.docx
 
-pandoc --from=gfm --to=docx --reference-doc=reference.docx --resource-path=practice_01 -o output\practice_01.docx practice_01\report.md
+pandoc --from=gfm --to=docx --reference-doc=reference.docx --resource-path=practice_01 -o output\practice_01.docx practice_01\README.md
 
 explorer.exe output
