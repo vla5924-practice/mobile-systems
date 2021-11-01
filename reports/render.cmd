39fe020276034466eb0bf4f3fb94b3ad34284fa6
vla5924 <vla5924@gmail.com>
2021-11-01 16:08:20 +0300

    updated render script

--- reports/render.cmd
+++ reports/render.cmd
@@ -4,5 +4,7 @@ del /q output\*.docx
 
 pandoc --from=gfm --to=docx --reference-doc=reference.docx --resource-path=practice_01 -o output\practice_01.docx practice_01\README.md
 pandoc --from=gfm --to=docx --reference-doc=reference.docx --resource-path=practice_02 -o output\practice_02.docx practice_02\README.md
+pandoc --from=gfm --to=docx --reference-doc=reference.docx --resource-path=practice_03 -o output\practice_03.docx practice_03\README.md
+pandoc --from=gfm --to=docx --reference-doc=reference.docx --resource-path=practice_04 -o output\practice_04.docx practice_04\README.md
 
 explorer.exe output
